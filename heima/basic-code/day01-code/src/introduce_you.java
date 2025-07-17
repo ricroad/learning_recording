4af84a91b9acb0803c00574ff6f0e2dd392a641c
yanzu_-admin <f52586666@gmail.com>
2025-07-17 15:44:00 +0800

    keep learn

---
+++ heima/basic-code/day01-code/src/introduce_you.java
@@ -0,0 +1,8 @@
+public class introduce_you {
+    public static void main(String[] args) {
+        System.out.println("姓名：彦祖");
+        System.out.println("性别：男");
+        System.out.println();
+        System.out.println("地址：湖北");
+    }
+}
